14ba3b87ded81274ac5362b069509c3e12c72c7f
Jingyi21 <qianwumeng_2cyndi@qq.com>
2018-09-10 04:06:06 +0800

    Update EditModule

---
+++ options.js
@@ -0,0 +1,108 @@
+var setHeader = {
+    background: [
+        'https://ww1.sinaimg.cn/large/0060lm7Tly1fjfi896lfbj31hc0u079s.jpg',
+        'https://ww2.sinaimg.cn/large/0060lm7Tly1fjfivy5o6zj31hc0u0wli.jpg',
+        'https://ww1.sinaimg.cn/large/0060lm7Tly1fjfivzadbwj31kw0w0k4s.jpg',
+        'https://ww1.sinaimg.cn/large/0060lm7Tly1fjfi88qcdpj31hc0u00xy.jpg',
+        'https://ww2.sinaimg.cn/large/0060lm7Tly1fji5691db2j31hc0u0n5v.jpg',
+        'https://ww4.sinaimg.cn/large/0060lm7Tly1fji5688e62j31hc0u0wjg.jpg',
+        'https://ww4.sinaimg.cn/large/0060lm7Tly1fjfi87rzn0j31hc0u0djp.jpg',
+        'https://ws3.sinaimg.cn/large/007giGeVly1fuvsdqd6duj31hc0u0aea.jpg',
+        'https://ws2.sinaimg.cn/large/0065BpO8gy1fuhnokeop1j31hc0u0too.jpg',
+        'https://ws4.sinaimg.cn/large/007giGeVly1fuvsgpyiplj31hc0u0adj.jpg'
+    ],
+    avatar: 'avatar.png'
+}
+
+var setLeftSection =  {
+     icon: './assets/avatar.jpg',
+     links: ['Home','Archives','Game','About']
+}
+
+var setRightSectionHome = [
+    {
+        icon: 'fa fa-home',
+        link: 'https://miya.moe',
+        value: 'Blog'
+    },{
+        icon: 'fa fa-home',
+        link: 'https://miya.moe',
+        value: 'Blog'
+    },{
+        icon: 'fa fa-home',
+        link: 'https://miya.moe',
+        value: 'Blog'
+    },{
+        icon: 'fa fa-home',
+        link: 'https://miya.moe',
+        value: 'Blog'
+    },{
+        icon: 'fa fa-home',
+        link: 'https://miya.moe',
+        value: 'Blog'
+    },{
+        icon: 'fa fa-home',
+        link: 'https://miya.moe',
+        value: 'Blog'
+    }];
+
+var setRightSectionArchives = [
+    {
+        link: 'https://miya.moe',
+        value: '测试项目',
+        date: '8102/9/9'
+    },{
+        link: 'https://miya.moe',
+        value: '测试项目',
+        date: '8102/9/9'
+    },{
+        link: 'https://miya.moe',
+        value: '测试项目',
+        date: '8102/9/9'
+    },{
+        link: 'https://miya.moe',
+        value: '测试项目',
+        date: '8102/9/9'
+    },{
+        link: 'https://miya.moe',
+        value: '测试项目',
+        date: '8102/9/9'
+    }];    
+
+var setRightSectionGames = [
+    {
+        name: 'Lovelive',
+        value: '123 456 789'
+    },{
+        name: 'Lovelive',
+        value: '123 456 789'
+    },{
+        name: 'Lovelive',
+        value: '123 456 789'
+    },{
+        name: 'Lovelive',
+        value: '123 456 789'
+    },{
+        name: 'Lovelive',
+        value: '123 456 789'
+    },{
+        name: 'Lovelive',
+        value: '123 456 789'
+    }];
+
+var setRightSection = {
+    homeTitle: 'Miya Neko House',
+    archivesTitle: 'Archives',
+    gamesTitle: 'Kacha',
+    about: {
+        title: '测试用标题',
+        text: '热闹是她们的,我什么都没有'
+    }
+}
+
+
+
+
+
+
+export { setHeader,setLeftSection,setRightSection,setRightSectionHome,setRightSectionArchives,setRightSectionGames }
